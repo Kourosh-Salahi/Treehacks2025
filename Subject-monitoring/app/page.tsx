0179d02c43909b9f288b3422c7fbbf295615431c
Kourosh Salahi <ksalahi@berkeley.edu>
2025-02-16 08:48:45 -0800

    changed title

--- Subject-monitoring/app/page.tsx
+++ Subject-monitoring/app/page.tsx
@@ -8,14 +8,14 @@ const SubjectIdInput = dynamic(() => import("./components/SubjectIdInput"), {
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold mb-8">Welcome to Active Subject Monitoring</h1>
+      <h1 className="text-4xl font-bold mb-8">Avengers Assemble</h1>
       <SubjectIdInput />
       <Link href="/overview" className="mt-4 text-blue-600 hover:underline">
         View Subject Status Overview
       </Link>
 
       <Link href="/livestream" className="mt-4 text-blue-600 hover:underline">
-        View Connected Heartrate Data
+        View Connected Heart rate Data
       </Link>
 
       
